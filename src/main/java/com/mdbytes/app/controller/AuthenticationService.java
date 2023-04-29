2eca685352f6467d789f8447caa2f2056f615d1e
martindwyer <martin@mdbytes.com>
2023-04-29 11:28:20 -0500

    init commit

---
+++ src/main/java/com/mdbytes/app/controller/AuthenticationService.java
@@ -0,0 +1,48 @@
+package com.mdbytes.app.controller;
+
+import com.mdbytes.app.dao.UserRepository;
+import com.mdbytes.app.entity.Role;
+import com.mdbytes.app.entity.User;
+import com.mdbytes.app.security.JwtService;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = "new user added";
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = "user authenticated";
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
---
+++ src/main/java/com/mdbytes/app/entity/Role.java
@@ -0,0 +1,6 @@
+package com.mdbytes.app.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
